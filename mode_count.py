9f2d21cd6295effeb8a402f8e57b0f21923e44ed
Asif-102 <aiqbal517@gmail.com>
2021-11-14 02:22:30 +0600

    mode_count

--- mode_count.py
+++ mode_count.py
@@ -1,12 +1,12 @@
+def mode(list):
+    wkts_freq = {}
+    for item in list:
+        if item in wkts_freq:
+            wkts_freq[item] += 1
+        else:
+            wkts_freq[item] = 1
+        print("Wicket:", item, "Count:", wkts_freq[item])
+
 wkts_list = [6,5,4,3,1,3,2,1,0,5,3,3,2,2,1,3,4,3,3]
 
-wkts_freq = {}
-
-for item in wkts_list:
-    if item in wkts_freq:
-        wkts_freq[item] += 1
-    else:
-        wkts_freq[item] = 1
-
-for item in wkts_freq:
-    print("Wicket:", item, "Count:", wkts_freq[item])
+mode(wkts_list)
